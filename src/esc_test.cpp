9f397e958e08c2ba949ffe048172d6e79b8aeb9b
Fernando Aragon <faragonchavarria3@gmail.com>
2026-06-23 18:08:52 -0400

    allowed for the esc signal to be sent to 4 pins

--- src/esc_test.cpp
+++ src/esc_test.cpp
@@ -1,23 +1,27 @@
 #include <Arduino.h>
 
-const int escPin = 18;
+const int fbESCPin = 18;
+const int fwESCPin = 25;
+const int bbESCPin = 26;
+const int bwESCPin = 27;
+
 const int channel = 0;
 const int freq = 50;      // 50 Hz
 const int resolution = 16;
 
-void writeESC(int us)
-{
+void writeESC(int us) {
     uint32_t duty = (uint32_t)((float)us / 20000.0 * ((1 << resolution) - 1));
     ledcWrite(channel, duty);
 }
 
-void setup()
-{
-
-    Serial.begin(9600);
+void setup() {
+    Serial.begin(115200);
     Serial.print("Test");
     ledcSetup(channel, freq, resolution);
-    ledcAttachPin(escPin, channel);
+    ledcAttachPin(fbESCPin, channel);
+    ledcAttachPin(fwESCPin, channel);
+    ledcAttachPin(bbESCPin, channel);
+    ledcAttachPin(bwESCPin, channel);
 
     // Send minimum throttle
     writeESC(1000);
@@ -26,8 +30,7 @@ void setup()
     delay(5000);
 }
 
-void loop()
-{
+void loop() {
     writeESC(1200);
     delay(2000);
 
